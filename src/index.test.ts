1a7df8da8efa15dcc809bb2787332debce9205bd
Ryota Kameoka <ok@ryota-ka.me>
2022-12-27 20:01:30 +0900

    Support `basePath` option

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,58 +4,161 @@ import * as request from 'supertest';
 
 import { pathCanonicalizer } from './';
 
+function mkApp(basePath?: string) {
+    const app = new Koa();
+    app.use(pathCanonicalizer({ basePath }));
+    app.use(async (ctx, next) => {
+        if (ctx.path === '/' || ctx.path === '/api/v1/users/42') {
+            ctx.status = 200;
+            ctx.body = 'Hello, world!';
+            await next();
+        }
+    });
+
+    return app;
+}
+
 describe(pathCanonicalizer, () => {
-    let app: Koa;
-    let server: Server;
+    describe('when the base path is not given', () => {
+        let app: Koa;
+        let server: Server;
 
-    beforeAll(() => {
-        app = new Koa();
-        app.use(pathCanonicalizer());
-        app.use(async (ctx, next) => {
-            if (ctx.path === '/' || ctx.path === '/api/v1/users/42') {
-                ctx.status = 200;
-                ctx.body = 'Hello, world!';
-                await next();
-            }
+        beforeAll(() => {
+            app = mkApp();
+            server = app.listen();
         });
 
-        server = app.listen();
+        afterAll(() => server.close());
+
+        const get = (path: string) => request(server).get(path);
+
+        describe('when the path is canonical', () => {
+            describe('/', () => {
+                it('returns 200', async () => {
+                    const res = await get('/');
+                    expect(res.status).toBe(200);
+                });
+            });
+
+            describe('/api/v1/users/42', () => {
+                it('returns 200', async () => {
+                    const res = await get('/api/v1/users/42');
+                    expect(res.status).toBe(200);
+                });
+            });
+        });
+
+        describe('when the path is non-canonical', () => {
+            describe('//', () => {
+                it('redirects to /', async () => {
+                    const res = await get('//');
+                    expect(res.status).toBe(302);
+                    expect(res.header['location']).toBe('/');
+                });
+            });
+
+            describe('//api////v1/users////42', () => {
+                it('redirects to /api/v1/users/42', async () => {
+                    const res = await get('//api////v1/users////42');
+                    expect(res.status).toBe(302);
+                    expect(res.header['location']).toBe('/api/v1/users/42');
+                });
+            });
+        });
     });
 
-    afterAll(() => server.close());
+    describe('when the base path is /admin', () => {
+        let app: Koa;
+        let server: Server;
 
-    const get = (path: string) => request(server).get(path);
+        beforeAll(() => {
+            app = mkApp('/admin');
+            server = app.listen();
+        });
 
-    describe('when the path is canonical', () => {
-        describe('/', () => {
-            it('returns 200', async () => {
-                const res = await get('/');
-                expect(res.status).toBe(200);
+        afterAll(() => server.close());
+
+        const get = (path: string) => request(server).get(path);
+
+        describe('when the path is canonical', () => {
+            describe('/', () => {
+                it('returns 200', async () => {
+                    const res = await get('/');
+                    expect(res.status).toBe(200);
+                });
+            });
+
+            describe('/api/v1/users/42', () => {
+                it('returns 200', async () => {
+                    const res = await get('/api/v1/users/42');
+                    expect(res.status).toBe(200);
+                });
             });
         });
 
-        describe('/api/v1/users/42', () => {
-            it('returns 200', async () => {
-                const res = await get('/api/v1/users/42');
-                expect(res.status).toBe(200);
+        describe('when the path is non-canonical', () => {
+            describe('//', () => {
+                it('redirects to /', async () => {
+                    const res = await get('//');
+                    expect(res.status).toBe(302);
+                    expect(res.header['location']).toBe('/admin');
+                });
+            });
+
+            describe('//api////v1/users////42', () => {
+                it('redirects to /api/v1/users/42', async () => {
+                    const res = await get('//api////v1/users////42');
+                    expect(res.status).toBe(302);
+                    expect(res.header['location']).toBe('/admin/api/v1/users/42');
+                });
             });
         });
     });
 
-    describe('when the path is non-canonical', () => {
-        describe('//', () => {
-            it('redirects to /', async () => {
-                const res = await get('//');
-                expect(res.status).toBe(302);
-                expect(res.header['location']).toBe('/');
+    describe('when the base path is /admin/', () => {
+        let app: Koa;
+        let server: Server;
+
+        beforeAll(() => {
+            app = mkApp('/admin/');
+            server = app.listen();
+        });
+
+        afterAll(() => server.close());
+
+        const get = (path: string) => request(server).get(path);
+
+        describe('when the path is canonical', () => {
+            describe('/', () => {
+                it('returns 200', async () => {
+                    const res = await get('/');
+                    expect(res.status).toBe(200);
+                });
+            });
+
+            describe('/api/v1/users/42', () => {
+                it('returns 200', async () => {
+                    const res = await get('/api/v1/users/42');
+                    expect(res.status).toBe(200);
+                });
             });
         });
 
-        describe('//api////v1/users////42', () => {
-            it('redirects to /api/v1/users/42', async () => {
-                const res = await get('//api////v1/users////42');
-                expect(res.status).toBe(302);
-                expect(res.header['location']).toBe('/api/v1/users/42');
+        describe('when the path is non-canonical', () => {
+            describe('//', () => {
+                it('redirects to /', async () => {
+                    const res = await get('//');
+                    expect(res.status).toBe(302);
+                    expect(res.header['location']).toBe('/admin/');
+                });
+            });
+
+            describe('//api////v1/users////42', () => {
+                it('redirects to /api/v1/users/42', async () => {
+                    const res = await get('//api////v1/users////42');
+                    expect(res.status).toBe(302);
+                    expect(res.header['location']).toBe('/admin/api/v1/users/42');
+                });
             });
         });
     });
--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,32 @@ import { Middleware, Context } from 'koa';
 
 import { canonicalize } from './canonicalize';
 
+type Params = Readonly<{
+    basePath?: string | undefined;
+}>;
+
 /**
  * A Koa middleware to redirect to canonicalized path
  */
-export function pathCanonicalizer(): Middleware {
+export function pathCanonicalizer(params: Params = {}): Middleware {
     return async function (ctx: Context, next: () => Promise<void>): Promise<void> {
         const newPath = canonicalize(ctx.path);
 
         if (newPath === null) {
             await next();
         } else {
-            ctx.redirect(newPath);
+            if (params.basePath === undefined) {
+                ctx.redirect(newPath);
+            } else {
+                if (newPath === '/') {
+                    ctx.redirect(params.basePath);
+                    return;
+                }
+
+                const basePath = params.basePath.endsWith('/') ? params.basePath.slice(0, -1) : params.basePath;
+
+                ctx.redirect(basePath + newPath);
+            }
         }
     };
 }
